d15ac91ce90441a5d5c10d99480a066404b417c7
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-28 12:42:50 +0200

    Add some style to navigation bar for phone version

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,20 +1,28 @@
 import NavLinks from "./NavLinks";
 import Logo from '../assets/logo.png';
 import NavButtons from './NavButtons';
+import '../styles/navigation.scss';
+import { BsMoon } from 'react-icons/bs';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const NavigationBar = () => (
   <nav className="navigation-bar">
     <div className="navigation-header">
-      <img className="navigation-logo" src={Logo} alt='koach logo'/>
+      <img className="navigation-logo" src={Logo} alt="koach logo" />
     </div>
     <div className="navigation-links">
       <NavLinks />
     </div>
-    <div className="navigation-dark-mode">
-      <button className="dar-mode-button" type="button"></button>
-    </div>
-    <div className="navigation-buttons">
-      <NavButtons />
+    <div className='navigation-right-phone'>
+      <div className="navigation-dark-mode">
+        <button className="dark-mode-button" type="button">
+          <BsMoon />
+        </button>
+      </div>
+      <div className="navigation-buttons">
+        <NavButtons />
+      </div>
+      <RxHamburgerMenu />
     </div>
   </nav>
 )
