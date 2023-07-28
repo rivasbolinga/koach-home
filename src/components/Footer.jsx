5b2280eecc038c40447fe7718e83df223c559f13
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-28 17:42:13 +0200

    Add footer skeleton

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-left-container">
+      <h1>Koach</h1>
+      <p>
+        We match the right initiatives with the right expertise, fostering
+        flexible connections and empowering success
+      </p>
+      <div className="social-logos">logos</div>
+    </div>
+    <div className="footer-right-container">
+      <div className="footer-links-container">
+        <h3>PLATFORM</h3>
+        <ul>
+          <li>Browse mentors</li>
+          <li>Book a Session</li>
+          <li>Become a Mentor</li>
+        </ul>
+      </div>
+      <div className="footer-links-container">
+        <h3>RESOURCES</h3>
+        <ul>
+          <li>Newsletter</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+      <div className="footer-links-container">
+        <h3>COMPANY</h3>
+        <ul>
+          <li>About</li>
+          <li>Partner Program</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+      <div className="footer-links-container">
+        <h3>SUPPORT</h3>
+        <ul>
+          <li>FAQ</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer;
